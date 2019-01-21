0421b342c23f2e69a2b31102921e7f6e1fc12f89
Dana Simmons <dsanalog@gmail.com>
2019-01-21 22:16:05 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,12 +4,11 @@ def prime?(num)
   num = num.abs 
   
   i = 2
-  while i < num do
+  while i < num
     if num % i == 0
       return false
     end
-    i++
+    i = i + 1 
   end
-  true
     
 end
